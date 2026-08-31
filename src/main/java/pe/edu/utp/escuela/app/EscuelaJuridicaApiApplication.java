e875ae647f43346eb2a8e0a6118f450e1c5b02c9
Enrique Prada <enrique.pdg@gmail.com>
2026-08-31 02:19:24 -0500

    chore: crear estructura

---
+++ src/main/java/pe/edu/utp/escuela/app/EscuelaJuridicaApiApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.escuela.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscuelaJuridicaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscuelaJuridicaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/utp/escuela/app/EscuelaJuridicaApiApplicationTests.java
@@ -0,0 +1,15 @@
+package pe.edu.utp.escuela.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class EscuelaJuridicaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
